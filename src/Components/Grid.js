4388130c24380f76c5b34c704acff2e459cf85f4
Vyper248 <vyper375@hotmail.com>
2021-02-18 15:27:36 +0000

    Added Grid component

---
+++ src/Components/Grid.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Grid = styled.div`
+    display: grid;
+    justify-items: center;
+    grid-template-columns: ${props => props.columns !== undefined ? props.columns : '1fr 1fr 1fr'};
+`;
+
+export default Grid;
