9ca2b0e59029e8fe63a0fc9bab570c5171e5515d
Vyper248 <vyper375@hotmail.com>
2021-03-12 15:06:57 +0000

    Removed sync option from Settings

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -7,7 +7,6 @@ import Button from '../Components/Button';
 import Websites from './Websites';
 import Categories from './Categories';
 import Items from './Items';
-import Sync from './Sync';
 import Backup from './Backup';
 
 const Settings = () => {
@@ -27,13 +26,11 @@ const Settings = () => {
             <Button value="Websites" onClick={onChangePage('Websites')} selected={page === 'Websites' ? true : false}/>
             <Button value="Categories" onClick={onChangePage('Categories')} selected={page === 'Categories' ? true : false}/>
             <Button value="Items" onClick={onChangePage('Items')} selected={page === 'Items' ? true : false}/>
-            <Button value="Sync" onClick={onChangePage('Sync')} selected={page === 'Sync' ? true : false}/>
             <Button value="Backup" onClick={onChangePage('Backup')} selected={page === 'Backup' ? true : false}/>
 
             { page === 'Websites' ? <Websites/> : null }
             { page === 'Categories' ? <Categories/> : null }
             { page === 'Items' ? <Items/> : null }
-            { page === 'Sync' ? <Sync/> : null }
             { page === 'Backup' ? <Backup/> : null }
         </div>
     );
